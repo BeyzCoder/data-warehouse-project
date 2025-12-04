389c65cf65d2aaa0d95d4a4bafb56457569c358b
Steven <82733217+BeyzCoder@users.noreply.github.com>
2025-12-04 13:13:23 -0600

    Update data_check_silver.sql

--- scripts/data_check_silver.sql
+++ scripts/data_check_silver.sql
@@ -65,3 +65,35 @@ FROM silver.crm_prd_info
 SELECT *
 FROM silver.crm_prd_info
 WHERE prd_end_dt > prd_start_dt
+
+
+/* The issues sls_order_dt */
+SELECT
+	sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8 
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+-- Check for invalid date order
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data ConsistencyZ: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative.
+SELECT
+sls_ord_num,
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price is NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+/* THERE ARE CUSTOMER ID THAT ARE NOT IN CSUTOMER_INFO */
+--WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_customer_info)
